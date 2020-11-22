ef19e3083b0cc9406f4d04298f74511a98e2c2cd
ryvvntong <rem.water@hotmail.com>
2020-11-22 15:00:32 -0500

    moving asteroids added

---
+++ src/asteroids/Asteroid.java
@@ -0,0 +1,16 @@
+package asteroids;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Asteroids extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/asteroids/AsteroidFactory.java
@@ -0,0 +1,4 @@
+package asteroids;
+
+public class AsteroidFactory {
+}
